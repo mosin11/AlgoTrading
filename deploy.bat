204a6bbbf8500074301bb6a6bbcadcfe979d3c72
mosin11 <mrtradingview766@gmail.com>
2024-08-24 12:21:40 +0530

    web soclet addedd

--- deploy.bat
+++ deploy.bat
@@ -31,19 +31,29 @@ REM npm install -g angular-cli-ghpages
 REM ng deploy --base-href=https://mosin11.github.io/AlgoTrading/
 REM Wait for 5 seconds before proceeding
 timeout /t 5 /nobreak
+echo "Building the project..."
 ng build --configuration production --base-href /AlgoTrading/
 
 REM Wait for 5 seconds before proceeding
 timeout /t 5 /nobreak
 
+
+
+echo "Deploying to GitHub Pages..."
+
 npx angular-cli-ghpages --dir=dist/frontend/browser
+
+npx angular-cli-ghpages --dir=dist/frontend/browser > deploy_log.txt 2>&1
+
 
 REM Wait for 5 seconds before proceeding
 timeout /t 5 /nobreak
 
 REM Open the deployed site in the default web browser
+echo "Deployment complete. Opening the site..."
 start https://mosin11.github.io/AlgoTrading/
 
 
 
+npx angular-cli-ghpages --dir=dist/frontend/browser > deploy_log.txt 2>&1
 

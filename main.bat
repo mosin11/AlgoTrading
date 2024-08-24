b5a9105a5e9be90d415d20d075e393cc7e7ccdc7
mosin11 <mrtradingview766@gmail.com>
2024-08-24 12:35:39 +0530

    web socket added

--- main.bat
+++ main.bat
@@ -2,32 +2,33 @@
 
 REM Initialize a new Git repository
 git init
-REM Add all files to the staging area
 git add .
-REM Commit the changes
-git commit -m "web soclet addedd"
-REM Rename the default branch to 'main'
+git commit -m "web socket added"
 git branch -M main
+
 REM Wait for 5 seconds before proceeding
 timeout /t 5 /nobreak
+
 REM Add remote repositories
 git remote add origin https://github.com/mosin11/AlgoTrading.git
-REM Wait for 5 seconds before proceeding
 timeout /t 5 /nobreak
-REM Push changes to the remote repository
 git push -u origin main
+
 REM Run npm predeploy
 REM npm run predeploy
+
 REM Run npm deploy
 REM npm run deploy
+
+REM Install angular-cli-ghpages if needed
 REM npm install -g angular-cli-ghpages
-REM ng deploy --base-href=https://mosin11.github.io/AlgoTrading/
+
 REM Wait for 5 seconds before proceeding
 timeout /t 5 /nobreak
+
+REM Build the project
 echo "Building the project..."
-ng build --configuration production --base-href /AlgoTrading/
-REM Wait for 5 seconds before proceeding
-timeout /t 5 /nobreak
+ng build --configuration production --base-href /AlgoTrading/ > build_log.txt 2>&1
 
 REM Check if the build command succeeded
 if %ERRORLEVEL% neq 0 (
@@ -42,3 +43,5 @@ REM Call the deployment batch file
 echo "Calling deploy script..."
 call deploy.bat
 
+REM Exit the script
+exit /b 0

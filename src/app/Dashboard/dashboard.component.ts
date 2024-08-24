4f55efe4ffa52be6fa1a6d642b856a7b935b083e
mosin11 <mrtradingview766@gmail.com>
2024-08-24 12:47:14 +0530

    web socket added

--- src/app/Dashboard/dashboard.component.ts
+++ src/app/Dashboard/dashboard.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { KotakWebSocketService } from '../services/kotak-websocket.service';
+import { OptionChainComponent } from "../option-chain/option-chain.component";
 
 @Component({
   selector: 'app-dashboard',
   standalone: true,
-  imports: [CommonModule, FormsModule],
+  imports: [CommonModule, FormsModule, OptionChainComponent],
   templateUrl: './dashboard.component.html',
   styleUrls: ['./dashboard.component.css']
 })
